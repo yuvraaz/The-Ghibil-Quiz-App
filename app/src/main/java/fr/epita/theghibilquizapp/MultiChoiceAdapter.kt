27cd58fe15de925ee62d45285e36b4898e845b6b
Youbaraj Poudel <youbaraj.poudel@talan.com>
2020-01-22 16:10:37 +0100

    Initial setup

---
+++ app/src/main/java/fr/epita/theghibilquizapp/MultiChoiceAdapter.kt
@@ -0,0 +1,49 @@
+package fr.epita.theghibilquizapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MultiChoiceAdapter(items: ArrayList<Choices>) :
+    RecyclerView.Adapter<MultiChoiceAdapter.CategoryViewHolder>() {
+    var catItems = items
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.single_item_choice, parent, false)
+        return CategoryViewHolder(itemView = view)
+    }
+
+    override fun getItemCount(): Int {
+        return catItems.count()
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        var choice: Choices = catItems[position]
+        holder.title.text = choice.title
+
+        //event
+        holder.title.setOnClickListener {
+            onListItemClickListener?.onItemClicked(choice)
+        }
+    }
+
+    class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var title: TextView = itemView.findViewById(R.id.lbl_title)
+
+    }
+
+    //Interface for item click listener
+
+    fun setOnItemClickListener(listener: OnListItemClickListener) {
+        this.onListItemClickListener = listener
+    }
+
+    var onListItemClickListener: OnListItemClickListener? = null
+
+    interface OnListItemClickListener {
+        fun onItemClicked(choice: Choices)
+    }
+}
---
+++ app/src/main/java/fr/epita/theghibilquizapp/QuizPageActivity.kt
@@ -0,0 +1,12 @@
+package fr.epita.theghibilquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
