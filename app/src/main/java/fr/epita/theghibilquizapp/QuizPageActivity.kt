9db8a7f8eb339897bb5f01cbd023db6223b119a4
Youbaraj Poudel <youbaraj.poudel@talan.com>
2020-01-22 17:01:25 +0100

    Network request success

--- app/src/main/java/fr/epita/theghibilquizapp/QuizPageActivity.kt
+++ app/src/main/java/fr/epita/theghibilquizapp/QuizPageActivity.kt
@@ -1,12 +1,39 @@
 package fr.epita.theghibilquizapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import okio.IOException
+
 
 class QuizPageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestFilms()
+    }
+
+    private fun requestFilms() {
+        val client = OkHttpClient()
+        val request: Request = Request.Builder()
+            .url("https://ghibliapi.herokuapp.com/films")
+            .build()
+
+
+        //Api call
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: java.io.IOException) {
+                Log.d("error", e.message)
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                Log.d("response", response.toString())
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 }
+
